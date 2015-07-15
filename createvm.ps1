1462c464eae42043177d705420a13629496dfced
paul198204 <paul1982.04@gmail.com>
2015-07-15 18:53:04 +0530

    I added new file cratevm.ps1

---
+++ createvm.ps1
@@ -0,0 +1,7 @@
+$vm = New-AzureVMConfig -Name "example-1" -InstanceSize Small -ImageName "a699494373c04fc0bc8f2bb1389d6106__Windows-Server-2012-Datacenter-201503.01-en.us-127GB.vhd"  
+
+$vm = Add-AzureProvisioningConfig -VM $vm -Windows -AdminUsername "user2" -Password "user2" 
+
+$vm = Set-AzureVMDSCExtension -VM $vm -ConfigurationArchive "IISInstall.ps1.zip" -ConfigurationName "IISInstall"  
+
+New-AzureVM -VM $vm -Location "Southeast Asia" -ServiceName "example-1-svc" -WaitForBoot
